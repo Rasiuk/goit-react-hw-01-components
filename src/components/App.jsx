bdc49732e0293535d903ccd10bf969e9f52680fb
Evgeniy <gerardcobain1@gmail.com>
2023-02-16 13:16:47 +0200

    first template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,17 @@ import { Profile } from './Profile/Profile';
 import user from '../user.json';
 import data from '../data.json';
 import friends from '../friends.json';
+import transactions from '../transactions.json';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendsList/FriendsList';
+import { TransactionsList } from './Transactions/TransactionsList';
 export const App = () => {
   return (
     <div>
       <Profile item={user} />
       <Statistics title="Upload stats" stats={data} />
       <FriendList items={friends} />
+      <TransactionsList items={transactions} />
     </div>
   );
 };
